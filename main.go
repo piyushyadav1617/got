2aedec1ea62a8337bb2628e9a78f02b2d18576d6
Piyush Yadav <yadavpiyush222@gmail.com>
2025-11-02 12:35:17 +0000

    adds ls-tree command

--- main.go
+++ main.go
@@ -11,6 +11,14 @@ import (
 	"os"
 )
 
+var gitModes = map[string]string{
+	"040000": "tree",
+	"100644": "blob",
+	"100755": "blob",
+	"120000": "blob",
+	"160000": "commit",
+}
+
 func main() {
 	if len(os.Args) < 2 {
 		fmt.Fprintf(os.Stderr, "usage: got <command> [<args>...]\n")
@@ -110,6 +118,94 @@ func main() {
 		if err := os.WriteFile(objectPath, buf.Bytes(), 0644); err != nil {
 			handleError(err)
 		}
+	case "ls-tree":
+		if len(os.Args) < 3 {
+			handleError(errors.New("usage: got ls-tree [<args>...] [hash]"))
+		}
+
+		var nameOnly bool
+		var hash string
+
+		if os.Args[2] == "--name-only" {
+			nameOnly = true
+			hash = os.Args[3]
+		} else {
+			hash = os.Args[2]
+		}
+
+		if len(hash) != 40 {
+			handleError(errors.New("invalid hash"))
+		}
+
+		filePath := fmt.Sprintf(".git/objects/%s/%s", hash[:2], hash[2:])
+		b, err := os.ReadFile(filePath)
+		if err != nil {
+			handleError(err)
+		}
+
+		r, err := zlib.NewReader(bytes.NewReader(b))
+		if err != nil {
+			handleError(err)
+		}
+		defer r.Close()
+
+		content, err := io.ReadAll(r)
+		if err != nil {
+			handleError(err)
+		}
+
+		nullIndex := bytes.IndexByte(content, 0)
+		if nullIndex == -1 {
+			handleError(errors.New("invalid git object format"))
+		}
+
+		type TreeEntry struct {
+			Mode string
+			Type string
+			Hash string
+			Name string
+		}
+
+		// Parse tree entries
+		var entries []TreeEntry
+		data := content[nullIndex+1:]
+		for len(data) > 0 {
+			// Parse mode
+			spaceIdx := bytes.IndexByte(data, ' ')
+			if spaceIdx == -1 {
+				handleError(errors.New("malformed entry: missing mode"))
+			}
+			mode := string(data[:spaceIdx])
+			data = data[spaceIdx+1:]
+
+			nullIdx := bytes.IndexByte(data, 0)
+			if nullIdx == -1 {
+				handleError(errors.New("malformed entry: missing name terminator"))
+			}
+			name := string(data[:nullIdx])
+			data = data[nullIdx+1:]
+
+			if len(data) < 20 {
+				handleError(errors.New("malformed entry: incomplete hash"))
+			}
+			hashBytes := data[:20]
+			data = data[20:]
+
+			entries = append(entries, TreeEntry{
+				Mode: mode,
+				Type: gitModes[mode],
+				Hash: sha1toHex(hashBytes),
+				Name: name,
+			})
+		}
+
+		for _, entry := range entries {
+			if nameOnly {
+				fmt.Println(entry.Name)
+			} else {
+				fmt.Printf("%s %s %s %s\n", entry.Mode, entry.Type, entry.Hash, entry.Name)
+			}
+		}
 
 	default:
 		fmt.Fprintf(os.Stderr, "Unknown command %s\n", command)
@@ -123,8 +219,10 @@ func handleError(err error) {
 		os.Exit(1)
 	}
 }
-
 func computeHash(data []byte) string {
 	h := sha1.Sum(data)
 	return hex.EncodeToString(h[:])
+}
+func sha1toHex(sha1sum []byte) string {
+	return hex.EncodeToString(sha1sum)
 }
